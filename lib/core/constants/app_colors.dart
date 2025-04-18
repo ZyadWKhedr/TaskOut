412b2959adfbc7ccd4a590ad16d0be6b5d62a53a
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-18 14:53:12 +0200

    first commit

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xffFAFCFF);
+  static const Color mainColor = Color(0xff00468C);
+  static const Color secondaryColor = Color(0xff0080FF);
+}
