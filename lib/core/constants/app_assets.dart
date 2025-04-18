009b5d1b44067f222903da6616700ed8269a91f9
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-18 14:58:25 +0200

    added app images

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static const String onboarding1 = 'assets/images/Frame 31.png';
+  static const String onboarding2 = 'assets/images/Frame 31 (1).png';
+  static const String onboarding3 = 'assets/images/Frame 34.png';
+  static const String login = 'assets/images/Frame 28.png';
+}
