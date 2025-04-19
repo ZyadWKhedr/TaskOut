b440d4529895fb0e88ee966e7b1538731c5703d5
Zyad Wael Khedr <zyadwkhedr@gmail.com>
2025-04-19 12:41:15 +0200

    finished auth logic with some of needed widgets

---
+++ lib/core/di.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:task_out/features/auth/data/repositories/auth_repo_impl.dart';
+
+import '../features/auth/domain/repositories/auth_repository.dart';
+
+final supabaseProvider = Provider((ref) => Supabase.instance.client);
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepoImpl(ref.watch(supabaseProvider));
+});
